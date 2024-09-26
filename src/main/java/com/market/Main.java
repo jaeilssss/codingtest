4121f52e5f831e0797e9e27b93469ffbb99e340d
jaeilssss <jaeilssss@bitbucket.org>
2024-09-26 22:37:23 +0900

    백준 16918 붐버맨 구현,시뮬레이션

--- src/main/java/com/market/Main.java
+++ src/main/java/com/market/Main.java
@@ -1,5 +1,6 @@
 package com.market;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -9,16 +10,60 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String s = scanner.next();
-
-        HashSet<String> set = new HashSet<>();
-
-        for(int i = 0; i < s.length(); i++) {
-            for(int j= i+1 ; j <s.length()+1; j++) {
-                set.add(s.substring(i,j));
+        int r = scanner.nextInt();
+        int c = scanner.nextInt();
+        int n = scanner.nextInt();
+        String [][] arr = new String[r][c];
+        int [][] boomTime = new int[r][c];
+        for(int i = 0 ; i < r; i++) {
+            String s = scanner.next();
+            for(int j = 0 ; j < s.length(); j++) {
+                arr[i][j] = s.substring(j,j+1);
+                if(arr[i][j].equals("O")) {
+                    boomTime[i][j] = 3;
+                }
             }
         }
 
-        System.out.println(set.size());
+        int [] dx = {0,1,-1,0};
+        int [] dy = {1,0,0,-1};
+
+       for(int t = 1; t <=n; t++) {
+            if(t%2==0) {
+                for(int i =0 ; i<r; i++) {
+                    for(int j = 0; j<c; j++) {
+                        if(arr[i][j].equals(".")) {
+                            arr[i][j] = "O";
+                            boomTime[i][j] = t+3;
+                        }
+                    }
+                }
+            } else {
+                for(int i = 0; i<r; i++) {
+                    for(int j =0; j <c; j++) {
+                        if(arr[i][j].equals("O") && t == boomTime[i][j]) {
+                            boomTime[i][j] = 0;
+                            arr[i][j] = ".";
+
+                            for(int k = 0; k <4; k++) {
+                                if((i+dx[k] >=0 && i+dx[k] <r) && (j+dy[k] >=0 && j+dy[k] <c)) {
+                                    if(t != boomTime[i+dx[k]][j+dy[k]]) {
+                                        boomTime[i+dx[k]][j+dy[k]] = 0;
+                                        arr[i+dx[k]][j+dy[k]] = ".";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+       }
+
+       for(int i = 0 ; i<r; i++){
+            for(int j = 0; j<c; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+       }
     }
 }
