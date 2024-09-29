b33d1bc97d884a5587a329c7eac5eae6cc06758d
jaeilssss <jaeilssss@bitbucket.org>
2024-09-29 17:39:58 +0900

    백준 개똥벌래 3020

--- src/main/java/com/market/Main.java
+++ src/main/java/com/market/Main.java
@@ -1,48 +1,47 @@
 package com.market;
 
-import java.awt.*;
 import java.util.*;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int H = scanner.nextInt();
 
-        String s = scanner.next();
-        StringBuilder answer = new StringBuilder();
-        String [] splitString = s.split("::");
+        int [] seoksoon = new int[H+1];
+        int [] jongyuseok = new int[H+1];
 
-        int gap = 8-s.split(":").length;
-        StringBuilder sb = new StringBuilder();
-        if(s.substring(0,2).equals("::")) {
-            for(int i = 0 ; i<gap+2; i++) {
-                sb.append("0000:");
-            }
-        } else {
-            for(int i = 0 ; i<gap+1; i++) {
-                sb.append(":0000:");
-            }
-        }
-        String temp  = sb.toString().replace("::",":");
-        s = s.replace("::", temp);
-        String [] splitString2 = s.split(":");
-
-        for(int j = 0; j<splitString2.length; j++) {
-            gap = 4- splitString2[j].length();
-            if(!splitString2[j].equals("")){
-                for(int k = 0; k<gap; k++) {
-                    splitString2[j] = "0"+splitString2[j];
-                }
+        for(int i = 0; i<N; i++) {
+            int h = scanner.nextInt();
+            if(i%2==1) {
+                jongyuseok[h]++;
+            } else {
+                seoksoon[h]++;
             }
         }
 
-        for(int i = 0 ; i<8; i++) {
-            if(i!=0) {
-                answer.append(":");
-            }
-            answer.append(splitString2[i]);
+        for(int i = H-1; i > 0; i--) {
+            seoksoon[i] += seoksoon[i+1];
         }
 
-        System.out.println(answer.toString());
+        for(int i = H-1; i > 0; i--) {
+            jongyuseok[i] += jongyuseok[i+1];
+        }
+
+        int minValue = Integer.MAX_VALUE;
+        int count = 0;
+        for(int i = 1; i < H+1; i++) {
+            int sum = seoksoon[i] + jongyuseok[H-i+1];
+
+            if(sum < minValue) {
+                minValue = sum;
+                count = 1;
+            } else if(sum == minValue){
+                count++;
+            }
+        }
+
+        System.out.println(minValue + " " + count);
     }
 }
