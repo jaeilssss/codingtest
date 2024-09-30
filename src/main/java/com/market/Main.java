6a76e9a86bc7790fc04cc93e45145d8d477e2296
jaeilssss <jaeilssss@bitbucket.org>
2024-09-30 22:56:08 +0900

    백준 게임 1072

--- src/main/java/com/market/Main.java
+++ src/main/java/com/market/Main.java
@@ -1,48 +1,32 @@
 package com.market;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
 
-        int count = 0;
+        long X = scanner.nextLong();
+        long Y = scanner.nextLong();
+        long winRate = (Y*100)/X;
+        int left = 1;
+        int right = 1000000000;
+        int answer = -1;
+        while (left <= right) {
+            int mid = (left + right) /2;
 
-        int[] arr = new int[N];
-        int start = 0;
-        int end = 0;
-        for (int i = 0; i < N; i++) {
-            arr[i] = scanner.nextInt();
-            if (start < arr[i]) start = arr[i];
-            end += arr[i];
-        }
+            long tempWinRate = ((Y+mid) *100) / (X+mid);
 
-        while (start <= end) {
-            count = 0;
-            int mid = (start + end) / 2;
-            int sum = 0;
-            for (int i = 0; i < N; i++) {
-                if (sum + arr[i] > mid) {
-                    count++;
-                    sum = 0;
-                }
-                sum += arr[i];
+            if(tempWinRate ==winRate) {
+                left = mid +1;
+            }else if(tempWinRate > winRate) {
+                answer = mid;
+                right = mid-1;
             }
 
-            if (sum != 0) {
-                count++;
-            }
-//            if (count == M) {
-//                answer = Integer.min(answer, mid);
-//                end = mid - 1;
-//            }
-            if (count > M) start = mid + 1;
-            else end = mid - 1;
-
         }
-        System.out.println(start);
+        System.out.println(answer);
     }
 }
