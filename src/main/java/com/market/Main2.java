829438d4451d5f031c5cf24fe20a04afa5afb1f2
jaeilssss <jaeilssss@bitbucket.org>
2024-10-01 16:55:30 +0900

    과제는 끝나지 않아 백준 17952

--- src/main/java/com/market/Main2.java
+++ src/main/java/com/market/Main2.java
@@ -2,65 +2,49 @@ package com.market;
 
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main2 {
     public static void main(String[] args)  {
-
         Scanner scanner = new Scanner(System.in);
 
-        int C = scanner.nextInt();
-        int R = scanner.nextInt();
-
-        int [][] arr = new int[R][C];
-
-        int UpC = 0;
-        int rightR = 0;
-        int downC = 0;
-        int leftR = R-1;
-
-        int [] dx = {-1,0,1,0};
-        int [] dy = {0,1,0,-1};
-
-        int count = 1;
-        int x = R;
-        int y = 0;
-        int dIndex = 0;
-
-        while(count <= C*R) {
-
-            while(true) {
-                if(count >C*R) break;
-
-                x += dx[dIndex];
-                y += dy[dIndex];
-                if(x<0 || y<0 || x>=R || y>=C) {
-                   x -= dx[dIndex];
-                   y -= dy[dIndex];
-                   break;
-                } else if(arr[x][y] != 0){
-                    x -= dx[dIndex];
-                    y -= dy[dIndex];
-                    break;
-                }
-                arr[x][y] = count++;
-            }
-            dIndex++;
-            dIndex %=4;
-
-        }
-       int n = scanner.nextInt();
-        if(n > C*R) System.out.println(0);
-        else {
-            for(int i = 0 ; i<R; i++) {
-                for(int j = 0; j<C; j++){
-                    if(arr[i][j] == n) {
-                        int answerX = R-i;
-                        int answerY = j+1;
-                        System.out.println(answerY+ " " + answerX);
+        int N = scanner.nextInt();
+        Stack<HomeWork> stack = new Stack<>();
+        int answer = 0;
+        for(int i = 0 ; i < N; i++) {
+            int a = scanner.nextInt();
+            if(a == 0) {
+                if(!stack.isEmpty()) {
+                    HomeWork homeWork = stack.pop();
+                    homeWork.t--;
+                    if(homeWork.t ==0){
+                        answer += homeWork.point;
+                        continue;
                     }
+                    stack.push(homeWork);
+
                 }
+            } else {
+                int point = scanner.nextInt();
+                int t = scanner.nextInt();
+                t--;
+                if(t==0) {
+                    answer += point;
+                    continue;
+                }
+                stack.push(new HomeWork(point, t));
             }
         }
+        System.out.println(answer);
+    }
 
+    static class HomeWork {
+        public int point;
+        public int t;
+
+        public HomeWork(int point, int t) {
+            this.point = point;
+            this.t = t;
+        }
     }
 }
